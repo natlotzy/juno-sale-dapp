69e72ca238082d2473f67377a126f69d7684286e
Natalie Lotz <natalie@honeybeehealth.com>
2021-09-28 10:26:15 -0700

    Add toast messages and use loader during purchase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,24 @@ import 'styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from 'components/Layout'
 import { SigningCosmWasmProvider } from 'contexts/cosmwasm'
+import { Provider as AlertProvider } from 'react-alert'
+import AlertTemplate from 'components/AlertTemplate'
+
+const options = {
+  position: 'bottom center',
+  timeout: 5000,
+  transition: 'scale',
+  containerStyle: { zIndex: 1040 }
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SigningCosmWasmProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <AlertProvider template={AlertTemplate} {...options}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AlertProvider>
     </SigningCosmWasmProvider>
   )
 }
